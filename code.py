2df24e8c295f4c96cd0b8b12ac54b8fb95714e58
illia <marketesh@gmail.com>
2021-11-30 12:28:10 +0200

    1.1

---
+++ code.py
@@ -0,0 +1,32 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup as BS
+
+def create_db():
+    # sql_text = """CREATE TABLE film (film_now TEXT NO NULL, date NUMBER REAL NOT NULL)"""
+    sql_text = """CREATE TABLE film (film_now TEXT NO NULL)"""
+    conn = sqlite3.connect('film.db')
+    curs = conn.cursor()
+    curs.execute(sql_text)
+    conn.close()
+
+def insert_data(film_now):
+    sql_text = """INSERT INTO film_now VALUES (?)"""
+    conn = sqlite3.connect("film.db")
+    curs = conn.cursor()
+    curs.execute(sql_text, (film_now))
+    conn.close()
+
+data = []
+
+r = requests.get("https://multiplex.ua/soon")
+html = BS(r.content, 'html.parser')
+
+for el in html.select(".soon_el > .el_right"):
+    title = el.select(".soon_fm_name")
+    data.append(title[0].text)
+
+# просто роздрукувати дані
+for item in data:
+    insert_data(item)
+
